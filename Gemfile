7d9dffc45a6503a0f236c01cdc093586f7713d8c
Danny Rosenblatt <dgrosenblatt@gmail.com>
2015-02-07 19:52:55 -0500

    Remove yahoo finance api; do it live instead

--- Gemfile
+++ Gemfile
@@ -8,7 +8,6 @@ gem 'haml'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
-gem 'yahoo-finance'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,9 +2,10 @@ class Stock < ActiveRecord::Base
   # all records created from seed file
   has_many :positions
 
+  # current price in cents from Yahoo Finance API
   def current_price
-    # yahoo finance api call yahoo.com/#{ticker}/format=json
-    # return price in cents
-    return 2500
+    uri = URI.parse("http://finance.yahoo.com/webservice/v1/symbols/#{ticker}/quote?format=json")
+    response = JSON.parse(Net::HTTP.get(uri))
+    (response["list"]["resources"][0]["resource"]["fields"]["price"].to_f*100).to_i
   end
 end
